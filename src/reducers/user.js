15ac6926ef2dccd4017da0f37411e20c187aa828
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-01-19 18:23:05 -0300

    player login reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,9 +1,16 @@
+import { PLAYER_LOGIN } from '../actions';
+
 const INITIAL_STATE = {
   email: '',
 };
 
 const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case PLAYER_LOGIN:
+    return ({
+      ...state,
+      email: action.payload,
+    });
   default:
     return state;
   }
