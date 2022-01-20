fb72592d4eef17152cf8579803bae2ec3c332233
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-01-20 10:46:49 -0300

    Action assincrona que busca dados das moedas na API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,11 @@
 export const PLAYER_LOGIN = 'PLAYER_LOGIN';
 export const playerLogin = (payload) => ({ type: PLAYER_LOGIN, payload });
+
+export const RECEIVE_CURRENCIES = 'RECEIVE_CURRENCIES';
+const receiveCurrencies = (payload) => ({ type: RECEIVE_CURRENCIES, payload });
+
+export const fetchCurrencies = () => (dispatch) => {
+  fetch('https://economia.awesomeapi.com.br/json/all')
+    .then((response) => response.json())
+    .then((currencyDatas) => dispatch(receiveCurrencies(currencyDatas)));
+};
