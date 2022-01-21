3976e73ebe63bda204743e27931b8a3b7e53ca08
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-01-21 19:02:34 -0300

    Logica do reducer para a action de adicionar despesas

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,4 +1,4 @@
-import { RECEIVE_CURRENCIES } from '../actions';
+import { ADD_EXPENSE, RECEIVE_CURRENCIES } from '../actions';
 
 const INITIAL_STATE = {
   currencies: [],
@@ -11,6 +11,11 @@ const wallet = (state = INITIAL_STATE, action) => {
     return ({
       ...state,
       currencies: action.payload,
+    });
+  case ADD_EXPENSE:
+    return ({
+      ...state,
+      expenses: [...state.expenses, action.payload],
     });
   default:
     return state;
