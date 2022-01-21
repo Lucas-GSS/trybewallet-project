634bbc9cfc718f36218e90b40d366cd3327df94e
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-01-21 19:04:29 -0300

    Insere no Header a soma das despesas

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,22 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.sumExpenses = this.sumExpenses.bind(this);
+  }
+
+  sumExpenses() {
+    const { expenses } = this.props;
+    if (expenses.length === 0) return 0;
+    return expenses.reduce((accumulator, expense) => {
+      accumulator += (
+        expense.value * expense.exchangeRates[expense.currency].ask
+      );
+      return accumulator;
+    }, 0);
+  }
+
   render() {
     const { email } = this.props;
     return (
@@ -16,7 +32,7 @@ class Header extends Component {
         <span>
           Total:
           {' '}
-          <span data-testid="total-field">0</span>
+          <span data-testid="total-field">{ this.sumExpenses().toFixed(2) }</span>
         </span>
         <span data-testid="header-currency-field">BRL</span>
       </header>
@@ -26,10 +42,12 @@ class Header extends Component {
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  expenses: state.wallet.expenses,
 });
 
 Header.propTypes = {
   email: PropTypes.string.isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default connect(mapStateToProps)(Header);
