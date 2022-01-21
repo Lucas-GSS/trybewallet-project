24e73bcc107312db3a155c7aaf784ef0bb176026
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-01-21 20:48:38 -0300

    Tabela de registro dos gastos

---
+++ src/components/ExpenseTable.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { deleteExpense } from '../actions';
+
+class ExpenseTable extends Component {
+  constructor(props) {
+    super(props);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleDelete({ target }) {
+    const { id } = target.parentElement.parentElement;
+    const { dispatchDelete } = this.props;
+    dispatchDelete(parseInt(id, 10));
+    target.remove(target.parentElement.parentElement);
+  }
+
+  render() {
+    const { expenses } = this.props;
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            expenses.length > 0 && expenses.map((
+              { id, value, description, currency, method, tag, exchangeRates },
+            ) => (
+              <tr id={ id } key={ id }>
+                <td>{ description }</td>
+                <td>{ tag }</td>
+                <td>{ method }</td>
+                <td>{ `${currency} ${parseFloat(value).toFixed(2)}` }</td>
+                <td>{ exchangeRates[currency].name.split('/')[0] }</td>
+                <td>{ parseFloat(exchangeRates[currency].ask).toFixed(2) }</td>
+                <td>
+                  { `R$ ${
+                    (parseFloat(value) * parseFloat(exchangeRates[currency].ask))
+                      .toFixed(2)}`}
+                </td>
+                <td>Real</td>
+                <td>
+                  <button type="button">Editar</button>
+                  <button
+                    data-testid="delete-btn"
+                    type="button"
+                    onClick={ this.handleDelete }
+                  >
+                    Excluir
+                  </button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchDelete: (id) => dispatch(deleteExpense(id)),
+});
+
+ExpenseTable.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.string).isRequired,
+  dispatchDelete: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseTable);
