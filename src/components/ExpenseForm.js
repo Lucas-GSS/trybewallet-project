d46aa1a7c90d3a1c095837d6e529d5db09fbf6f6
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-01-21 19:03:32 -0300

    Formulario de novas despesas

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addExpense, fetchCurrencies } from '../actions';
+import CurrencyInput from './CurrencyInput';
+import DescriptionInput from './DescriptionInput';
+import ValueInput from './ValueInput';
+import MeansOfPayment from './MeansOfPayment';
+import TagInput from './TagInput';
+import AddExpenseButton from './AddExpenseButton';
+
+class ExpenseForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: 0,
+      value: '',
+      description: '',
+      currency: 'USD',
+      method: 'Dinheiro',
+      tag: 'Alimentação',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  async componentDidMount() {
+    const { dispatchFetchCurrencies } = this.props;
+    await dispatchFetchCurrencies();
+  }
+
+  async getExchangeRates() {
+    const getCurrencies = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const json = await getCurrencies.json();
+    const exchangeRates = await json;
+    return exchangeRates;
+  }
+
+  handleChange({ target: { id, value } }) {
+    this.setState({ [id]: value });
+  }
+
+  async handleClick() {
+    console.log(await this.getExchangeRates());
+    const { id } = this.state;
+    const { dispatchExpense } = this.props;
+    dispatchExpense(this.state, await this.getExchangeRates());
+    this.setState({
+      id: id + 1,
+      value: '',
+      description: '',
+      currency: 'USD',
+      method: 'Dinheiro',
+      tag: 'Alimentação',
+    });
+  }
+
+  render() {
+    const { currency, value, description, method, tag } = this.state;
+    return (
+      <form>
+        <ValueInput value={ value } handleChange={ this.handleChange } />
+        <DescriptionInput
+          description={ description }
+          handleChange={ this.handleChange }
+        />
+        <CurrencyInput
+          handleChange={ this.handleChange }
+          defaultCurrency={ currency }
+        />
+        <MeansOfPayment handleChange={ this.handleChange } method={ method } />
+        <TagInput tag={ tag } handleChange={ this.handleChange } />
+        <AddExpenseButton handleClick={ this.handleClick } />
+      </form>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchFetchCurrencies: () => dispatch(fetchCurrencies()),
+  dispatchExpense: (state, exchangeRates) => dispatch(addExpense(state, exchangeRates)),
+});
+
+ExpenseForm.propTypes = {
+  dispatchFetchCurrencies: PropTypes.func.isRequired,
+  dispatchExpense: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(ExpenseForm);
